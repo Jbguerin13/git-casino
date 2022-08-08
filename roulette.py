9478df325673a527c8a0bb5af5fb1764a183bc42
Tigrus1995 <jeanbaptisteguerin1@outlook.fr>
2022-08-08 18:19:23 +0200

    v2 simplification, correction

--- roulette.py
+++ roulette.py
@@ -1,90 +1,93 @@
 import random
+import numpy
 
-def rouletteNombre (betUser,money): #boucle pour répétition si user sort de l'intervalle [1,36]   
-    choiceUser=eval(input("Sur quel numéro voulez-vous poser votre jeton de 1 à 36 :"))
-    while choiceUser <1 or choiceUser >36: 
-        choiceUser=eval(input("Sur quel numéro voulez-vous poser votre jeton de 1 à 36 :"))
-    result=random.randrange(0,36)
+#Beginner code of the casino game Roulette in french Version. Code review by PhG
+
+def roulette_number (bet_user, money): #loop to repeat if the user get out from [1,36] 
+    choices_user = int(input("Sur quel numéro voulez-vous poser votre jeton de 1 à 36 :"))#security tips : never use "eval" again
+    while choices_user <1 or choices_user >36: 
+        choices_user = int(input("Sur quel numéro voulez-vous poser votre jeton de 1 à 36 :"))
+    result = random.randrange(0, 36)
     print("La bille est tombée sur le", result)
-    if result==0:
+    if result==0 :
         print("la bille est tombée sur le 0, vous avez perdu")
         return money
-    if choiceUser==result:
-            betUser=betUser*36
-            money=money+betUser
-            print("vous avez gagné",betUser,"€")
+    if choices_user == result :
+            bet_user = bet_user * 36
+            money = money + bet_user
+            print(f"vous avez gagné {bet_user} €") #tips : print(f"blabla {fct} blabla")
             return money
     else:
-            print("vous avez perdu", betUser, "€")
+            print(f"vous avez perdu {bet_user} €")
             return money
 
-def rouletteStart(): #demande la mise
+def ask_bet_amount(): #ask the bet
     print("faites vos jeux !")
-    betUser=eval(input("Entrer la mise en € : "))
-    return betUser
+    bet_user = int(input("Entrer la mise en € : "))
+    return bet_user
 
-def roulettePair(betUser,money): #jeu pair/impair
-    PI=["Pair","impair"]
-    choiceUser=eval(input("Précisez votre choix : Pair=0, Impair=1 : "))
-    result=random.randrange(0,36)
-    print("la bille est tombée sur le :",result)
-    if result==0:
+def roulette_peer_odd(bet_user, money): #game peer/odd
+    PI = ["Pair","impair"]
+    choices_user = int(input("Précisez votre choix : Pair=0, Impair=1 : "))
+    result = random.randrange(0,36)
+    print(f"la bille est tombée sur le :{result}")
+    if result == 0:
         print("la bille est tombée sur le 0, vous avez perdu")
         return money
-    result=result%2
-    print("Le résultat est",PI[result])
-    if result==choiceUser:
-        betUser=betUser*2
-        money=money+betUser
-        print("vous avez gagné", betUser,"€")
+    result = result % 2
+    print(f"Le résultat est {PI[result]}")
+    if result == choices_user :
+        bet_user = bet_user * 2
+        money = money + bet_user
+        print(f"vous avez gagné {bet_user} €")
         return money
     else:
-        print("Vous avez perdu", betUser,"€")
+        print(f"Vous avez perdu {bet_user} €")
         return money
 
-def rouletteCouleur(betUser,money): #Jeu couleur
-    couleur=["vert","rouge","noir","rouge","noir","rouge","noir","rouge","noir","rouge","noir","noir","rouge","noir","rouge","noir","rouge","noir","rouge","rouge","noir","rouge","noir","rouge","noir","rouge","noir","rouge","noir","noir","rouge","noir","rouge","noir","rouge","noir","rouge"]
-    choiceUser=input("Précisez votre choix rouge ou noir ? : ")
-    result=random.randrange(0,36)
-    print("La bille est tombée sur", result,"de couleur",couleur[result])
-    if result==0:
+def roulette_color(bet_user, money) : #game color
+    choices_user = input("Précisez votre choix rouge ou noir ? : ")
+    result = numpy.random.choice(["vert", "rouge", "noir"], p = [1/37, 18/37, 18/37]) #using numpy to simplify
+    print(f"La bille est tombée sur {result}")
+    if result == 0 :
         print("la bille est tombée sur le 0, vous avez perdu")
         return money
-    if couleur[result]==choiceUser:
-        betUser=betUser*2
-        money=money+betUser
-        print("vous avez gagné",betUser,"€")
+    if result == choices_user:
+        bet_user = bet_user * 2
+        money = money + bet_user
+        print(f"vous avez gagné {bet_user} €")
         return money
     else:
-        print("Vous avez perdu", betUser,"€")
+        print(f"Vous avez perdu {bet_user} €")
         return money
 
-def roulette(betUser,money): 
-    choiceGame=eval(input("Sur quels jeux voulez-vous placer votre mise ? Couleur=0 Pair/Impair=1 Nombres=2 :"))
-    if choiceGame==0:
-        return rouletteCouleur(betUser,money)
-    elif choiceGame==1:
-        return roulettePair(betUser,money)
-    elif choiceGame==2:
-        return rouletteNombre(betUser,money)
+def roulette(bet_user, money) :
+    ROULETTE_CHOICES = { #Using dictionnary to avoid "if forest"
+    0 : roulette_color,
+    1 : roulette_peer_odd,
+    2 : roulette_number,
+    }
+    choice_game = int(input("Sur quels jeux voulez-vous placer votre mise ? Couleur=0 Pair/Impair=1 Nombres=2 :"))
+    return ROULETTE_CHOICES[choice_game](bet_user,money)
+
 
 
 #lancement du code
-nameUser=input("Bienvenue dans les casinos en lignes de JBG !! Entrez votre prénom/nom : ") 
+nameUser = input("Bienvenue dans les casinos en lignes de JBG !! Entrez votre prénom/nom : ") 
 print("Nous espérons que le sort vous soit favorable", nameUser, ";)")
-money=eval(input("Combiens désirez vous mettre dans votre bourse en € : "))
+money = eval(input("Combiens désirez vous mettre dans votre bourse en € : "))
 print("c'est parti !!")
-replay=0
-while replay==0:
-    betUser=rouletteStart()
-    money=money-betUser
-    money=roulette(betUser,money)
-    print("Il vous reste", money," € dans votre bourse")
-    if money<=0:
-        replay=1
+replay = 0
+while replay == 0:
+    bet_user = ask_bet_amount()
+    money = money - bet_user
+    money = roulette(bet_user,money)
+    print(f"Il vous reste {money} € dans votre bourse")
+    if money <= 0 :
+        replay = 1
         print("Vous n'avez plus de sous dans votre bourse :(, revenez au début du mois prochain ;)")
     else:
-        replay=eval(input("Souhaitez-vous rejouer ? oui=0 non=1 :"))
+        replay = int(input("Souhaitez-vous rejouer ? oui=0 non=1 :"))
 
 
 
